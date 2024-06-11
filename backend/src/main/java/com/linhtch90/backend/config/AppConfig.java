501aa1fc1b09338c39f76d18cce53aad41c8b084
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2024-06-11 21:15:57 +0530

    Update AppConfig.java

--- backend/src/main/java/com/linhtch90/backend/config/AppConfig.java
+++ backend/src/main/java/com/linhtch90/backend/config/AppConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+
+//test comment
 @Component
 public class AppConfig {
     @Bean
