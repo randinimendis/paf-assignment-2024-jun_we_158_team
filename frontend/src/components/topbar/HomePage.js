b39804491585ccfb2b45dc910633c81b6a0949ca
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2024-10-12 21:58:00 +0530

    frontend_commit

---
+++ frontend/src/components/topbar/HomePage.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import {Link, useNavigate} from "react-router-dom";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+import {
+  BsFillPersonPlusFill,
+ 
+} from "react-icons/bs";
+
+import {RiLoginBoxLine} from "react-icons/ri";
+
+import styles from "./styles/HomePage.module.css";
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("psnToken") !== null) {
+      navigate("/newsfeed");
+    }
+  });
+
+  return (
+    <Container fluid>
+      <Row className={styles.container}>
+        <Col className={styles.colContainerRight}>
+          <div className={styles.colWithButtons}>
+            <Row>
+              <Link to="/signin" className={styles.linkTextFormat}><Button variant="primary" className={`${styles.btnHomePage} mb-3`}>Sign In <RiLoginBoxLine /></Button></Link>
+            </Row>
+            <Row>
+            <Link to="/signup" className={styles.linkTextFormat}><Button variant="primary" className={styles.btnHomePage}>Sign Up <BsFillPersonPlusFill /></Button></Link>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default HomePage;
